15747741c13cc53288ed3641ca81bd48e4205d8e
Mehdi Mazraehmolaie <61406816+mmolaie@users.noreply.github.com>
2023-01-20 13:29:37 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 53:53/udp --name dns -v "$(pwd)/Corefile":/tmp/Corefile -v "/zone":/tmp/zone coredns
+docker run -d -p 53:53/udp --name dns -v "$(pwd)/Corefile":/tmp/Corefile -v $(pwd):/tmp/zone coredns
